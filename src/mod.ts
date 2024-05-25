09b2a434372425abee4b4e6ad2e33f7f108b8a9b
dzmitry.s <s.dmitrii.89@gmail.com>
2024-05-25 14:55:53 +0200

    working version

---
+++ src/mod.ts
@@ -0,0 +1,161 @@
+import { DependencyContainer } from "tsyringe";
+
+// SPT types
+import { IPreAkiLoadMod } from "@spt-aki/models/external/IPreAkiLoadMod";
+import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
+import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
+import { PreAkiModLoader } from "@spt-aki/loaders/PreAkiModLoader";
+import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+import { ImageRouter } from "@spt-aki/routers/ImageRouter";
+import { ConfigServer } from "@spt-aki/servers/ConfigServer";
+import { ConfigTypes } from "@spt-aki/models/enums/ConfigTypes";
+import { ITraderConfig } from "@spt-aki/models/spt/config/ITraderConfig";
+import { IRagfairConfig } from "@spt-aki/models/spt/config/IRagfairConfig";
+import { JsonUtil } from "@spt-aki/utils/JsonUtil";
+
+// New trader settings
+import * as baseJson from "../db/base.json";
+import { TraderHelper } from "./traderHelpers";
+import { FluentAssortConstructor as FluentAssortCreator } from "./fluentTraderAssortCreator";
+import { Money } from "@spt-aki/models/enums/Money";
+import { Traders } from "@spt-aki/models/enums/Traders";
+import { HashUtil } from "@spt-aki/utils/HashUtil";
+
+import { Item } from "@spt-aki/models/eft/common/tables/IItem";
+import { IDatabaseTables } from "@spt-aki/models/spt/server/IDatabaseTables";
+
+
+import * as assortmentJson from "../db/assort.json";
+interface RawAssortmentItem {
+    _id: string;
+    _tpl: string;
+    parentId?: string;
+    slotId?: string;
+}
+
+class SampleTrader implements IPreAkiLoadMod, IPostDBLoadMod
+{
+    private mod: string
+    private logger: ILogger
+    private traderHelper: TraderHelper
+    private fluentAssortCreator: FluentAssortCreator
+
+    constructor() {
+        this.mod = "akguyshopts"; // Set name of mod so we can log it to console later
+    }
+
+    /**
+     * Some work needs to be done prior to SPT code being loaded, registering the profile image + setting trader update time inside the trader config json
+     * @param container Dependency container
+     */
+    public preAkiLoad(container: DependencyContainer): void
+    {
+        // Get a logger
+        this.logger = container.resolve<ILogger>("WinstonLogger");
+        this.logger.debug(`[${this.mod}] preAki Loading... `);
+
+        // Get SPT code/data we need later
+        const preAkiModLoader: PreAkiModLoader = container.resolve<PreAkiModLoader>("PreAkiModLoader");
+        const imageRouter: ImageRouter = container.resolve<ImageRouter>("ImageRouter");
+        const hashUtil: HashUtil = container.resolve<HashUtil>("HashUtil");
+        const configServer = container.resolve<ConfigServer>("ConfigServer");
+        const traderConfig: ITraderConfig = configServer.getConfig<ITraderConfig>(ConfigTypes.TRADER);
+        const ragfairConfig = configServer.getConfig<IRagfairConfig>(ConfigTypes.RAGFAIR);
+
+        // Create helper class and use it to register our traders image/icon + set its stock refresh time
+        this.traderHelper = new TraderHelper();
+        this.fluentAssortCreator = new FluentAssortCreator(hashUtil, this.logger);
+        this.traderHelper.registerProfileImage(baseJson, this.mod, preAkiModLoader, imageRouter, "AKGUY.jpg");
+        this.traderHelper.setTraderUpdateTime(traderConfig, baseJson, 3600, 4000);
+
+        // Add trader to trader enum
+        Traders[baseJson._id] = baseJson._id;
+
+        // Add trader to flea market
+        ragfairConfig.traders[baseJson._id] = true;
+
+        this.logger.debug(`[${this.mod}] preAki Loaded`);
+    }
+    
+    /**
+     * Majority of trader-related work occurs after the aki database has been loaded but prior to SPT code being run
+     * @param container Dependency container
+     */
+    public postDBLoad(container: DependencyContainer): void
+    {
+        this.logger.debug(`[${this.mod}] postDb Loading... `);
+
+        // Resolve SPT classes we'll use
+        const databaseServer: DatabaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+        const configServer: ConfigServer = container.resolve<ConfigServer>("ConfigServer");
+        const jsonUtil: JsonUtil = container.resolve<JsonUtil>("JsonUtil");
+
+        // Get a reference to the database tables
+        const tables = databaseServer.getTables();
+
+        // Add new trader to the trader dictionary in DatabaseServer - has no assorts (items) yet
+        this.traderHelper.addTraderToDb(baseJson, tables, jsonUtil);
+
+        this.populateItems(tables);
+
+        // Add trader to locale file, ensures trader text shows properly on screen
+        // WARNING: adds the same text to ALL locales (e.g. chinese/french/english)
+        this.traderHelper.addTraderToLocales(baseJson, tables, baseJson.name + " " + baseJson.surname, baseJson.name, baseJson.nickname, baseJson.location, "Want some AK?");
+
+        this.logger.debug(`[${this.mod}] postDb Loaded`);
+    }
+
+    private populateItems(tables: IDatabaseTables) {
+        const rootRawItems: RawAssortmentItem[] = assortmentJson.items
+            .filter((item: RawAssortmentItem) => item.parentId === "hideout");
+
+        const itemCosts = Object.entries(assortmentJson.barter_scheme)
+            .reduce( (map, [key, value]) => {
+                map[key] = value[0][0].count;
+
+                return map;
+            }, {});
+
+        rootRawItems.forEach((rawItem: RawAssortmentItem) => {
+            const complexAssortItem: Item[] = this.buildComplexAssortItemRecursive(rawItem._id, []);
+
+            if (complexAssortItem.length === 1) {
+                this.fluentAssortCreator.createSingleAssortItem(rawItem._tpl)
+                    .addUnlimitedStackCount()
+                    .addMoneyCost(Money.ROUBLES, itemCosts[rawItem._id] || 1)
+                    .addLoyaltyLevel(1)
+                    .export(tables.traders[baseJson._id]);
+            } else {
+                console.log("Adding complex assort item " + rawItem._id + " with " + complexAssortItem.length + " components")
+                this.fluentAssortCreator
+                    .createComplexAssortItem(complexAssortItem)
+                    .addUnlimitedStackCount()
+                    .addMoneyCost(Money.ROUBLES, itemCosts[rawItem._id] || 1)
+                    .addLoyaltyLevel(1)
+                    .export(tables.traders[baseJson._id]);
+            }
+
+        })    
+    }
+
+    private buildComplexAssortItemRecursive(itemId: string, components: Item[]): Item[] 
+    {
+        const rootItem: Item = assortmentJson.items
+            .find((item: RawAssortmentItem) => item._id === itemId);
+        
+        components.push(rootItem);    
+
+        const newComponents = assortmentJson.items
+            .filter((item: RawAssortmentItem) => item.parentId === itemId)
+            .map((item: RawAssortmentItem): Item => item)        
+        
+        if (newComponents.length === 0) 
+        {
+            return components;
+        }
+
+        return newComponents.flatMap(root => this.buildComplexAssortItemRecursive(root._id, components));
+    }
+}
+
+module.exports = { mod: new SampleTrader() }
